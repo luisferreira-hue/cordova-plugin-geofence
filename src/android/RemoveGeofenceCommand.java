59c566bf89550af8a0375ab5a5eff98baef7db56
jmeijer <modulus>
2022-03-09 10:47:11 +0100

    NonNull removed

--- src/android/RemoveGeofenceCommand.java
+++ src/android/RemoveGeofenceCommand.java
@@ -1,7 +1,6 @@
 package com.cowbell.cordova.geofence;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
 import android.util.Log;
 
 import com.google.android.gms.location.GeofencingClient;
