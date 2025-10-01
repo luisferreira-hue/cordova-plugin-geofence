2d108a6f3bb78c810b9004df12a034d78b196185
luisferreira-hue <luis.ferreira@osquay.com>
2025-10-01 19:17:32 +0100

    Update GeofencePlugin.swift

--- src/ios/GeofencePlugin.swift
+++ src/ios/GeofencePlugin.swift
@@ -481,9 +481,7 @@ class GeoNotificationManager : NSObject, CLLocationManagerDelegate {
 
                 //Send a notification to the device
                 if geoNotification["notification"].isExists() {
-            		if !errorMessage.isEmpty {
-                			geoNotification["notification"]["text"] = errorMessage
-            		}
+            		
             		notifyAbout(geoNotification)
                 }
                 
